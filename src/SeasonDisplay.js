c573f426467ff35db9c2b9d68b8ced87e087116b
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-10-28 11:14:25 +0530

    Adding styling and loading indicator, and refactoring.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,21 +1,44 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
 
-const getSeason =(lat,month)=>{
-    if(month > 2 && month < 9){
-    return lat > 0 ? 'summer' : 'winter'
+
+const seasonConfig = {
+    summer: {
+        text: 'Lets hit the Beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burr, its chilly',
+        iconName: 'snowflake'
     }
-    else{
-        return lat>0 ?'winter': 'summer'
+};
+
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter'
+    }
+    else {
+        return lat > 0 ? 'winter' : 'summer'
     }
 
 }
 
-const SeasonDisplay = (props)=>{
-    const season= getSeason(props.lat, new Date().getMonth());
-    const text= season=== 'winter'? 'Burr, it is chilly': 'Lets hit the Beach'
-    
-    return <div><h1>{text}</h1></div>
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season]
+
+
+    return (
+        <div className={`season-display ${season}`} >
+            <i className={`icon-left massive ${iconName} icon`} />
+            <h1>
+                {text}
+            </h1>
+            <i className={`icon-right massive ${iconName} icon`} />
+        </div>
+    )
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";    
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
@@ -8,36 +9,39 @@ class App extends React.Component {
 
     // Equivalent to creating a constructor function and initializing our state from there
 
-    state= {lat:null, errorMessage:''};
+    state = { lat: null, errorMessage: '' };
 
-    componentDidMount(){
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position)=>  this.setState({lat:position.coords.latitude}),
-            (err) =>this.setState({errorMessage : err.message})
-            
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
+
         );
     }
 
+    renderContent(){
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage} </div>
+        }
 
-    render(){
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
 
-       if(this.state.errorMessage && !this.state.lat){
-           return <div> Error: {this.state.errorMessage} </div>
-       }
+        }
+        return <Spinner message="Please accept location request" />
 
-       if(!this.state.errorMessage && this.state.lat){
-           return <SeasonDisplay lat={this.state.lat}/>
-       }
-            return (
-            <div>
-               Loading!
+    }
+
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
             </div>
-        );
-
-                
+        )
     }
 }
 
 ReactDOM.render(
-    <App/>,document.querySelector('#root')
+    <App />, document.querySelector('#root')
 );
